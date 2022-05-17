a7755ca73a2f7b83d83db44165469ea79e8eefd1
Jesus898 <jesus81398@gmail.com>
2022-05-17 14:04:07 -0500

    firstcommit

---
+++ models/Users.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define("Users", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  Users.associate = (models) => {
+    Users.hasMany(models.Likes, {
+      onDeleted: "cascade",
+    });
+
+    Users.hasMany(models.Posts, {
+      onDeleted: "cascade",
+    });
+  };
+
+  return Users;
+};
